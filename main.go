839236b378ccec9859976f0788d397eff88493de
文余两人 <523272086@qq.com>
2019-05-31 20:10:00 +0800

    unit test

--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
-	proto "github.com/micro/go-config/source/grpc/proto"
+	"github.com/micro/go-config"
+	"github.com/micro/go-config/source/consul"
 	"github.com/micro/go-log"
-	"sync"
 )
 
-var (
-	mux        sync.RWMutex
-	configMaps = make(map[string]*proto.ChangeSet)
-	apps       = []string{"micro"}
-)
+type dbConfig struct {
+	DB int `json:"dict"`
+	//Ca int    `json:"port"`
+}
 
 func main() {
-
-	defer func() {
-		if r := recover(); r != nil {
-			log.Logf("[main] Recovered in f %v", r)
-		}
-	}()
-	err := loadAndWatchConfigFile()
-	if err != nil {
-		log.Fatal(err)
+	// 注册consul的配置地址
+	consulSource := consul.NewSource(
+		consul.WithAddress("127.0.0.1:8500"),
+		consul.WithPrefix("/micro/config/database"),
+		// optionally strip the provided prefix from the keys, defaults to false
+		consul.StripPrefix(true),
+	)
+	// 创建新的配置
+	conf := config.NewConfig()
+	if err := conf.Load(consulSource); err != nil {
+		log.Logf("load config errr!!!", err)
 	}
-
+	var d dbConfig
+	aa := conf.Map()
+	log.Log(aa)
+	if err := conf.Get("micro", "config", "database", "loop").Scan(&d); err != nil {
+		log.Logf("json format err!!!", err)
+	}
+	log.Log(d.DB)
 }
