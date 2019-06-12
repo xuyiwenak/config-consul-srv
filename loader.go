47bb6d4874f1102d1e170792c7f68c71f0ee9411
文余两人 <523272086@qq.com>
2019-06-12 15:56:41 +0800

    add http api to upload json

--- loader.go
+++ loader.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/micro/go-config"
+	"github.com/micro/go-config/encoder/json"
+	"github.com/micro/go-config/source"
 	"github.com/micro/go-config/source/file"
 	"github.com/micro/go-log"
-	"os"
+	"io/ioutil"
+	"net/http"
 	"path/filepath"
+	"strings"
 	"sync"
 )
 
 var (
 	configData []byte
+	jsdata     string
 	m          sync.RWMutex
 	inited     bool
 	err        error
@@ -21,7 +28,6 @@ func Init() {
 
 	m.Lock()
 	defer m.Unlock()
-
 	if inited {
 		log.Logf("[Init] 配置已经初始化过")
 		return
@@ -31,15 +37,36 @@ func Init() {
 	// 先加载基础配置
 	appPath, _ := filepath.Abs(filepath.Dir(filepath.Join("./", string(filepath.Separator))))
 
-	pt := filepath.Join(appPath, "conf")
-	os.Chdir(appPath)
-
+	e := json.NewEncoder()
+	fmt.Println(appPath)
+	fileSource := file.NewSource(
+		file.WithPath(appPath+"/conf/micro.yml"),
+		source.WithEncoder(e),
+	)
+	conf := config.NewConfig()
 	// 加载micro.yml文件
-	if err = config.Load(file.NewSource(file.WithPath(pt + "/micro.yml"))); err != nil {
+	if err = conf.Load(fileSource); err != nil {
 		panic(err)
 	}
+	fmt.Println(string(conf.Bytes()))
+	url := "http://127.0.0.1:8500/v1/kv/micro/config/cluster"
+
+	req, err := http.NewRequest("PUT", url, strings.NewReader(string(conf.Bytes())))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	defer func() {
+		if res != nil && res.Body != nil {
+			res.Body.Close()
+		}
+	}()
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(res)
+	fmt.Println(string(body))
 	// 侦听文件变动
-	watcher, err := config.Watch()
+	watcher, err := conf.Watch()
 	if err != nil {
 		log.Fatalf("[Init] 开始侦听应用配置文件变动 异常，%s", err)
 		panic(err)
@@ -52,15 +79,38 @@ func Init() {
 				log.Fatalf("[loadAndWatchConfigFile] 侦听应用配置文件变动 异常， %s", err)
 				return
 			}
-			if err := config.Get(appPath, "micro").Scan(&configData); err != nil {
+			if err = conf.Load(fileSource); err != nil {
 				panic(err)
 			}
-			log.Log(string(configData))
 			log.Logf("[loadAndWatchConfigFile] 文件变动，%s", string(v.Bytes()))
 		}
 	}()
-	log.Log(string(configData))
 	// 标记已经初始化
 	inited = true
 	return
+}
+func PutJson(url, body string) (ret string, err error, resp *http.Response) {
+	buf := bytes.NewBufferString(body)
+	req, err := http.NewRequest("PUT", url, buf)
+	if err != nil {
+		panic(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = http.DefaultClient.Do(req)
+	defer func() {
+		if resp != nil && resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+	if err != nil {
+		log.Log(err.Error())
+		return "", err, resp
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err, resp
+	}
+
+	return string(data), nil, resp
 }
