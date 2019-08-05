580a9234224500c62049117349edc7cd5407d309
文余两人 <523272086@qq.com>
2019-08-05 14:16:06 +0800

    修改测试例子的错误

--- client.go
+++ client.go
@@ -11,8 +11,9 @@ func main() {
 	// 注册consul的配置地址
 	consulSource := consul.NewSource(
 		consul.WithAddress("127.0.0.1:8500"),
-		consul.WithPrefix("/micro/config/cluster"),
-		// optionally strip the provided prefix from the keys, defaults to false
+		// 这个前缀截止到需要读取的kv前
+		consul.WithPrefix("/micro/config/"),
+		// 是否使用前缀默认值是关闭，如果不用Get的时候需要填写完整的路径
 		consul.StripPrefix(true),
 	)
 	// 创建新的配置
@@ -20,9 +21,11 @@ func main() {
 	if err := conf.Load(consulSource); err != nil {
 		log.Logf("load config errr!!!", err)
 	}
-	if err := conf.Get("micro", "config", "cluster"); err != nil {
-		log.Logf("json format err!!!", err)
-	}
+	// 可以直接读取到map里面
 	configMap := conf.Map()
 	fmt.Println(configMap)
+	// 也可以根据类型读取
+	stmMap := map[string]string{}
+	mapConf := conf.Get("cluster").StringMap(stmMap)
+	fmt.Printf("%v", mapConf)
 }
